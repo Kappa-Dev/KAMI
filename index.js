f65ca7da98d4d0906cedfb4d4a5770f704db9bf9
Adrien Basso-Blandin <cypher_dechu@hotmail.com>
2016-11-04 19:45:27 +0100

    update

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ define(["ressources/d3/d3.js","HierarchyFinder.js","HierarchyUpdater.js","Interr
 		var svg = new InterractiveGraph(main_ct_id,server_url,dispatch); 
 		hf.init("/");
 		dispatch.on("load.hrF",function(node){
-				svg.init(["/"]);
 		});
 		dispatch.on("statechange.hrF",function(path){
 			console.log("updating svg graph");
-				console.log(path);
 				svg.init(path);
 		});
 		
--- ressources/Convert.js
+++ ressources/Convert.js
@@ -1,7 +1,6 @@
 define([],function(){return {
 	absPath : function absPath(p){
 		var path = p.concat();
-		console.log("path : "+path);
 		path[0]="";
 		path.push("");
 		return path.join("/");	
