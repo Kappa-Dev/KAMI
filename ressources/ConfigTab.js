e5a606728697b6f6744465c18a6af3d8ac056576
Adrien Basso-Blandin <cypher_dechu@hotmail.com>
2016-12-16 15:39:36 +0100

    working on config

--- ressources/ConfigTab.js
+++ ressources/ConfigTab.js
@@ -5,11 +5,12 @@
  * this project is under AGPL Licence
 */
 define([
-	"ressources/d3/d3.js"
+	"ressources/d3/d3.js",
+	"ressources/requestFactory.js"
 	],
-	function(d3){
+	function(d3,Rfactory){
 	
-	return function(container,dispatch){
+	return function(container,dispatch,srv){
 		d3.select("#"+container).append("div")//add a gear button
 			.classed("config_icon",true)
 			.on("click",configShow)
@@ -20,7 +21,7 @@ define([
 		var server_url="";
 		this.init = function init(){
 			hieShape = {};
-			server_url="https://api.executableknowledge.org/iregraph";
+			server_url=srv;
 			config_container=d3.select("body").append("div")
 				.attr("id","config_menu")
 				.property("disabled",true)
@@ -33,6 +34,10 @@ define([
 		}
 		this.update = function update(graph_name){
 			delete hieShape[graph_name];
+			Rfactory.getConfig(graph_name,configDisplay(ret));
+		}
+		function configDisplay(req){
+			hieShape[req.name])=
 		}
 		this.loadGraphConf = function loadGraphConf(graph_name){
 			console.log(graph_name);
