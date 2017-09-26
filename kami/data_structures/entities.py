b32705cdd6fb17b2f2149a9839a28b676a878cdc
eugenia <yarutoua@gmail.com>
2017-09-26 15:20:37 +0200

    Filled up docstrings

--- kami/data_structures/entities.py
+++ kami/data_structures/entities.py
@@ -1,30 +1,40 @@
-"""Collection of classes implementing kami-specific entities."""
+"""Collection of classes implementing KAMI-specific entities.
+
+KAMI entites specify an intermediary representation format for defining
+agents of PPIs.
+
+The implemented classes include:
+
+- `Agent` represents a gene defined by the UniProt accession number.
+
+- `Region` represents a domain defined by !....!
+
+- `State` represents a state given by its name and value;
+
+- `Residue` represents a residue defined by an amino acid and
+(optionally) its location, it also encapsulates a `State` object
+corresponding to a state of this residue.
+
+- `PhysicalEntity` base class for physical entities in KAMI. Physical
+entities encapsulate info about PTMs (residues, states, bounds)
+
+- `PhysicalRegion` represents a physical region defined by a region
+and a set of residues, states and bounds.
+
+- `PhysicalAgent` represents a gene product, it can be thought of as
+a physical protein defined by an agent (some gene) and by a set of regions,
+residues, states and bounds (possible PTMs).
+
+- `PhysicalRegionAgent`
+
+- `NuggetAnnotation`
+
+"""
 import collections
 
 
-class PhysicalEntity(object):
-    """Base class for physical entities in KAMI."""
-    def _normalize_bounds(self, bounds):
-        new_bounds = []
-        for el in bounds:
-            if not isinstance(el, collections.Iterable):
-                new_bounds.append([el])
-            else:
-                new_bounds.append(el)
-        return new_bounds
-
-    def add_residue(self, residue):
-        pass
-
-    def add_state(self, state):
-        pass
-
-    def add_bound(self, partners):
-        pass
-
-
 class Agent(object):
-    """Class implementing kami agent."""
+    """Class implementing KAMI agent (gene)."""
 
     def __init__(self, uniprotid, names=None,
                  xrefs=None, location=None):
@@ -41,6 +51,7 @@ class Agent(object):
         self.location = location
 
     def __str__(self):
+        """String represenation of an agent."""
         return str(self.uniprotid)
 
     def to_attrs(self):
@@ -53,41 +64,11 @@ class Agent(object):
         return agent_attrs
 
 
-class PhysicalAgent(PhysicalEntity):
-    """Class implementing kami physical agent."""
-
-    def __init__(self, agent, regions=None,
-                 residues=None, states=None, bounds=None):
-        """Initialize kami physical agent object."""
-        self.agent = agent
-        if regions:
-            self.regions = regions
-        else:
-            self.regions = []
-        if residues:
-            self.residues = residues
-        else:
-            self.residues = []
-        if states:
-            self.states = states
-        else:
-            self.states = []
-        if bounds:
-            self.bounds = self._normalize_bounds(bounds)
-        else:
-            self.bounds = []
-        return
-
-    def __str__(self):
-        return str(self.agent)
-
-
 class Region(object):
-    """Class implementing kami region."""
+    """Class implementing KAMI region (domain)."""
 
     def __init__(self, start=None, end=None, name=None):
-        """Initialize kami region object.
-        """
+        """Initialize KAMI region object."""
         self.start = start
         self.end = end
         self.name = name
@@ -111,49 +92,31 @@ class Region(object):
         return res
 
 
-class PhysicalRegion(PhysicalEntity):
-    """."""
+class State(object):
+    """Class implementing KAMI state."""
 
-    def __init__(self, region, residues=None, states=None, bounds=None):
-        """Initialize kami physical region object."""
-        self.region = region
-        if residues:
-            self.residues = residues
-        else:
-            self.residues = []
-        if states:
-            self.states = states
-        else:
-            self.states = []
-        if bounds:
-            self.bounds = self._normalize_bounds(bounds)
-        else:
-            self.bounds = []
-        return
+    def __init__(self, name, value):
+        """Init kami state object."""
+        self.name = name
+        self.value = value
 
     def __str__(self):
-        return str(self.region)
-
-
-class PhysicalRegionAgent(PhysicalEntity):
-    """."""
-    def __init__(self, physical_region, physical_agent):
-        """."""
-        self.physical_region = physical_region
-        self.physical_agent = physical_agent
-
-    def __str__(self):
-        res = ""
-        res += str(self.physical_region) + " domain of " +\
-            str(self.physical_agent)
+        """Str representation of state."""
+        res = "%s" % (self.name)
         return res
+
+    def to_attrs(self):
+        """Convert agent object to attrs."""
+        return {
+            self.name: {self.value}
+        }
 
 
 class Residue(object):
-    """Class implementing kami residue."""
+    """Class implementing KAMI residue."""
 
     def __init__(self, aa, loc=None, state=None):
-        """Init kami residue object."""
+        """Init residue object."""
         if type(aa) == set:
             pass
         elif isinstance(aa, collections.Iterable):
@@ -179,24 +142,112 @@ class Residue(object):
         }
 
 
-class State(object):
-    """Class implementing kami state."""
+class PhysicalEntity(object):
+    """Base class for physical entities in KAMI.
 
-    def __init__(self, name, value):
-        """Init kami state object."""
-        self.name = name
-        self.value = value
+    Implements several methods of common behaviour:
+    - `add_residue` - adds a residue to a physical entity
+    - `add_state` - adds a state to a physical entity
+    - `add_bound` - adds a bound partner to a physical entity
+    """
+
+    def _normalize_bounds(self, bounds):
+        new_bounds = []
+        for el in bounds:
+            if not isinstance(el, collections.Iterable):
+                new_bounds.append([el])
+            else:
+                new_bounds.append(el)
+        return new_bounds
+
+    def add_residue(self, residue):
+        """Add a residue to a list of residues of the entity."""
+        self.residues.append(residue)
+        return
+
+    def add_state(self, state):
+        """Add a state to a list of residues of the entity."""
+        self.states.append(state)
+        return
+
+    def add_bound(self, partner):
+        """Add a bound to a list of residues of the entity."""
+        self.bounds.append(partner)
+        pass
+
+
+class PhysicalRegion(PhysicalEntity):
+    """Class implementing KAMI physical region (physical domain)."""
+
+    def __init__(self, region, residues=None, states=None, bounds=None):
+        """Initialize kami physical region object."""
+        self.region = region
+        if residues:
+            self.residues = residues
+        else:
+            self.residues = []
+        if states:
+            self.states = states
+        else:
+            self.states = []
+        if bounds:
+            self.bounds = self._normalize_bounds(bounds)
+        else:
+            self.bounds = []
+        return
 
     def __str__(self):
-        """Str representation of state."""
-        res = "%s" % (self.name)
+        """String representation of a physical region."""
+        return str(self.region)
+
+
+class PhysicalAgent(PhysicalEntity):
+    """Class implementing KAMI physical agent (protein product)."""
+
+    def __init__(self, agent, regions=None,
+                 residues=None, states=None, bounds=None):
+        """Initialize KAMI physical agent object."""
+        self.agent = agent
+        if regions:
+            self.regions = regions
+        else:
+            self.regions = []
+        if residues:
+            self.residues = residues
+        else:
+            self.residues = []
+        if states:
+            self.states = states
+        else:
+            self.states = []
+        if bounds:
+            self.bounds = self._normalize_bounds(bounds)
+        else:
+            self.bounds = []
+        return
+
+    def __str__(self):
+        """String representation of a physical agent."""
+        return str(self.agent)
+
+
+class PhysicalRegionAgent(PhysicalEntity):
+    """."""
+    def __init__(self, physical_region, physical_agent):
+        """."""
+        self.physical_region = physical_region
+        self.physical_agent = physical_agent
+
+    def __str__(self):
+        res = ""
+        res += str(self.physical_region) + " domain of " +\
+            str(self.physical_agent)
         return res
 
-    def to_attrs(self):
-        """Convert agent object to attrs."""
-        return {
-            self.name: {self.value}
-        }
+
+class Motif():
+    """."""
+    def __init__(self, )
 
 
 class NuggetAnnotation(object):
