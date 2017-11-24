d6d7c90c074bbd9d2b2e5f089ec894937de5310d
Eugenia <yarutoua@gmail.com>
2017-11-24 13:49:47 +0100

    Small fixes

--- kami/server/kami/anatomizer_tools.py
+++ kami/server/kami/anatomizer_tools.py
@@ -1,6 +1,7 @@
 import anatomizer.new_anatomizer as anatomizer
 import regraph.tree as tree
 
+
 def anatomizer_add_agent(hie, g_id, parent_id, uniprot_id):
     anatomy = anatomizer.GeneAnatomy(
         uniprot_id,
@@ -10,13 +11,12 @@ def anatomizer_add_agent(hie, g_id, parent_id, uniprot_id):
         offline=True
     )
     if anatomy.found:
-        agent_id = tree.add_node(hie, g_id, parent_id, uniprot_id ,"agent", new_name=True)
-        for region in anatomy.domains :
+        agent_id = tree.add_node(hie, g_id, parent_id, uniprot_id, "agent", new_name=True)
+        for region in anatomy.domains:
             region_id = region.short_names[0]
             region_type = region.feature_type
             if region_type == 'Domain' or region_type == 'Repeat':
-                new_region_id = tree.add_node(hie, g_id, parent_id, region_id ,"region", new_name=True)
+                new_region_id = tree.add_node(hie, g_id, parent_id, region_id, "region", new_name=True)
                 tree.add_edge(hie, g_id, parent_id, new_region_id, agent_id)
     else:
         raise ValueError("Entry %s not found" % uniprot_id)
-
