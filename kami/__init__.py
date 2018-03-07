a22b67e99327b36e125dc028a1e120dfa1d4e213
Eugenia Oshurko <yarutoua@gmail.com>
2018-03-07 09:40:33 +0100

    Aliases added

--- kami/__init__.py
+++ kami/__init__.py
@@ -1,5 +1,11 @@
 import logging
 
+from kami.entities import *
+from kami.hierarchy import KamiHierarchy
+from kami.interactions import *
+from kami.resolvers.black_box import add_interaction, add_interactions
+
+
 logging.basicConfig(
     format='%(levelname)s: kami/%(name)s - %(message)s', level=logging.INFO
 )
