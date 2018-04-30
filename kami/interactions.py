2db8ce7d3095fd9a0a18367ae228be4e3b99ca54
Eugenia Oshurko <yarutoua@gmail.com>
2018-04-30 15:47:23 +0200

    Added: Unbinding interaction, modified: directed attribute removed from interactions

--- kami/interactions.py
+++ kami/interactions.py
@@ -1,8 +1,5 @@
 """Collection of classes implementing interactions."""
-import collections
-
-from kami.entities import (Gene, SiteActor, RegionActor, Actor)
-from kami.exceptions import KamiError
+from kami.entities import (Gene, SiteActor, RegionActor)
 
 
 class Interaction(object):
@@ -15,7 +12,6 @@ class Interaction(object):
             attrs["rate"] = {self.rate}
         if self.annotation is not None:
             attrs["text"] = {self.annotation}
-        attrs["direct"] = {self.direct}
         return attrs
 
 
@@ -23,36 +19,27 @@ class Modification(Interaction):
     """Class for Kami mod interaction."""
 
     def __init__(self, enzyme, substrate, mod_target,
-                 mod_value=True, rate=None, annotation=None,
-                 direct=True):
+                 mod_value=True, rate=None, annotation=None):
         """Initialize modification."""
-        if not isinstance(enzyme, Actor):
-            raise KamiError(
-                "Enzyme of Modification interaction should be "
-                "an instance of 'kami.entities.Actor' class: "
-                "'%s' received instead" % type(enzyme))
-        if not isinstance(substrate, Actor):
-            raise KamiError(
-                "Substrate of Modification interaction should be "
-                "an instance of 'kami.entities.Actor' class: "
-                "'%s' received instead" % type(substrate))
         self.enzyme = enzyme
         self.substrate = substrate
         self.target = mod_target
         self.value = mod_value
         self.rate = rate
         self.annotation = annotation
-        self.direct = direct
         return
 
     def __str__(self):
         """String representation of Modification class."""
         res = "Modification:\n"
-        res += "\tEnzyme: %s\n" % self.enzyme
-        res += "\tSubstrate: %s\n" % self.substrate
-        res += "\tMod target: %s\n" % self.target
-        res += "\tValue: %s\n" % self.value
-        res += "\tDirect? %s\n" % self.direct
+        res += "\tEnzyme: {}\n".format(self.enzyme)
+        res += "\tSubstrate: {}\n".format(self.substrate)
+        res += "\tMod target: {}\n".format(self.target)
+        res += "\tValue: {}\n".format(self.value)
+        if self.rate is not None:
+            res += "\tRate: {}\n".format(self.rate)
+        if self.annotation is not None:
+            res += "\tAnnotation: {}\n".format(self.annotation)
         return res
 
     def __repr__(self):
@@ -62,8 +49,13 @@ class Modification(Interaction):
         mod_target = self.target.__repr__()
 
         res = "Modification(" +\
-            "enzyme={}, substrate={}, target={}, value={})".format(
+            "enzyme={}, substrate={}, target={}, value={}".format(
                 enzyme_rep, substrate_rep, mod_target, self.value)
+        if self.rate is not None:
+            res += ", rate={}".format(self.rate)
+        if self.annotation is not None:
+            res += ", annotation={}".format(str(self.annotation))
+        res += ")"
         return res
 
     def enzyme_site(self):
@@ -82,49 +74,74 @@ class Modification(Interaction):
 class Binding(Interaction):
     """Class for Kami binary binding interaction."""
 
-    def __init__(self, left_members, right_members,
-                 rate=None, annotation=None, direct=True):
+    def __init__(self, left, right,
+                 rate=None, annotation=None):
         """Initialize binary binding."""
-        if isinstance(left_members, collections.Iterable):
-            left_members = set(left_members)
-        else:
-            left_members = set([left_members])
-        if isinstance(right_members, collections.Iterable):
-            right_members = set(right_members)
-        else:
-            right_members = set([right_members])
-        self.left = left_members
-        self.right = right_members
+        self.left = left
+        self.right = right
         self.rate = rate
         self.annotation = annotation
-        self.direct = direct
 
     def __str__(self):
         """String representation of Binding class."""
-        res = "Binding:\n"
-        res += "\tLeft members: {}\n".format(
-            ", ".join([str(m) for m in self.left]))
-        res += "\tRight members: {}\n".format(
-            ", ".join([str(m) for m in self.right]))
-        res += "\tDirect? {}\n".format(self.direct)
+        res = "Binding: {} binds {}\n".format(str(self.left), str(self.right))
         if self.rate is not None:
             res += "\tRate: {}\n".format(self.rate)
+        if self.annotation is not None:
+            res += "\tAnnotation: {}\n".format(self.annotation)
         return res
 
     def __repr__(self):
         """Representation of a Binding object."""
-        res = "Binding(left=[{}], right=[{}])".format(
-            ", ".join(el.__repr__() for el in self.left),
-            ", ".join(el.__repr__() for el in self.right))
+        res = "Binding(left={}, right={}".format(
+            self.left.__repr__(), self.right.__repr__())
+        if self.rate is not None:
+            res += ", rate={}".format(self.rate)
+        if self.annotation is not None:
+            res += ", annotation={}".format(self.annotation)
+        res += ")"
+        return res
+
+
+class Unbinding(Interaction):
+    """Class for Kami unbinding interaction."""
+
+    def __init__(self, left, right,
+                 rate=None, annotation=None):
+        """Initialize unbinding."""
+        self.left = left
+        self.right = right
+        self.rate = rate
+        self.annotation = annotation
+
+    def __str__(self):
+        """String representation of Unbinding class."""
+        res = "Unbinding: {} unbinds {}\n".format(
+            str(self.left), str(self.right))
+        if self.rate is not None:
+            res += "\tRate: {}\n".format(self.rate)
+        if self.annotation is not None:
+            res += "\tAnnotation: {}\n".format(self.annotation)
+        return res
+
+    def __repr__(self):
+        """Representation of a Unbinding object."""
+        res = "Unbinding(left={}, right={}".format(
+            self.left.__repr__(), self.right.__repr__())
+        if self.rate is not None:
+            res += ", rate={}".format(self.rate)
+        if self.annotation is not None:
+            res += ", annotation={}".format(self.annotation)
+        res += ")"
         return res
 
 
 class AutoModification(Modification):
-    """Class for Kami auto mod interaction."""
+    """Class for Kami automodification interaction."""
 
     def __init__(self, enzyme, target, value=True,
                  substrate_region=None, substrate_site=None, rate=None,
-                 annotation=None, direct=True):
+                 annotation=None):
         """Initialize modification."""
         self.enzyme = enzyme
         self.substrate_region = substrate_region
@@ -133,11 +150,10 @@ class AutoModification(Modification):
         self.value = value
         self.rate = rate
         self.annotation = annotation
-        self.direct = direct
         return
 
     def __str__(self):
-        """String representation of AutoModification class."""
+        """String representation of an AutoModification object."""
         res = "Modification:\n"
         res += "\tEnzyme: {}\n".format(self.enzyme)
         if self.substrate_region is not None:
@@ -146,11 +162,14 @@ class AutoModification(Modification):
             res += "Substrate site: {}\n".format(self.substrate_site)
         res += "\tMod target: {}\n".format(self.target)
         res += "\tValue: {}\n".format(self.value)
-        res += "\tDirect? {}\n".format(self.direct)
+        if self.rate is not None:
+            res += "\tRate: {}\n".format(self.rate)
+        if self.annotation is not None:
+            res += "\tAnnotation: {}\n".format(self.annotation)
         return res
 
     def __repr__(self):
-        """Representation of a AutoModificationAutoModification object."""
+        """Representation of an AutoModification object."""
         enzyme_rep = self.enzyme.__repr__()
         mod_target = self.target.__repr__()
 
@@ -159,18 +178,22 @@ class AutoModification(Modification):
         if self.substrate_region is not None:
             res += ", substrate_region={}".format(
                 self.substrate_region.__repr__())
-        if self. substrate_site is not None:
+        if self.substrate_site is not None:
             res += ", substrate_site={}".format(
                 self.substrate_site.__repr__())
+        if self.rate is not None:
+            res += ", rate={}".format(self.rate)
+        if self.annotation is not None:
+            res += ", annotation={}".format(str(self.annotation))
         res += ")"
         return res
 
 
 class AnonymousModification(Modification):
-    """Class for Kami anonymous mod interaction."""
+    """Class for Kami anonymous modification interaction."""
 
     def __init__(self, substrate, target, value=True,
-                 rate=None, annotation=None, direct=True):
+                 rate=None, annotation=None):
         """Initialize modification."""
         self.enzyme = None
         self.substrate = substrate
@@ -178,25 +201,32 @@ class AnonymousModification(Modification):
         self.value = value
         self.rate = rate
         self.annotation = annotation
-        self.direct = direct
 
     def __str__(self):
-        """String representation of AnonymousModification class."""
+        """String representation of an AnonymousModification object."""
         res = "AnonymousModification:\n"
         res += "\tSubstrate: %s\n" % self.substrate
         res += "\tMod target: %s\n" % self.target
         res += "\tValue: %s\n" % self.value
-        res += "\tDirect? %s\n" % self.direct
+        if self.rate is not None:
+            res += "\tRate: {}\n".format(self.rate)
+        if self.annotation is not None:
+            res += "\tAnnotation: {}\n".format(self.annotation)
         return res
 
     def __repr__(self):
-        """Representation of a AnonymousModification object."""
+        """Representation of an AnonymousModification object."""
         substrate_rep = self.substrate.__repr__()
         mod_target = self.target.__repr__()
 
         res = "AnonymousModification(" +\
-            "enzyme=None, substrate={}, target={}, value={})".format(
+            "enzyme=None, substrate={}, target={}, value={}".format(
                 substrate_rep, mod_target, self.value)
+        if self.rate is not None:
+            res += ", rate={}".format(self.rate)
+        if self.annotation is not None:
+            res += ", annotation={}".format(self.annotation)
+        res += ")"
         return res
 
 
@@ -206,7 +236,7 @@ class TransModification(Modification):
     def __init__(self, enzyme, substrate, target, value=True,
                  enzyme_bnd_region=None, enzyme_bnd_site=None,
                  substrate_bnd_region=None, substrate_bnd_site=None,
-                 rate=None, annotation=None, direct=True):
+                 rate=None, annotation=None):
         """Initialize modification."""
         self.enzyme = enzyme
         self.substrate = substrate
@@ -218,16 +248,15 @@ class TransModification(Modification):
         self.substrate_bnd_site = substrate_bnd_site
         self.rate = rate
         self.annotation = annotation
-        self.direct = direct
         return
 
     def __str__(self):
         """String representation of TransModification class."""
         res = "TransModification:\n"
-        res += "\tEnzyme: %s\n" % self.enzyme
-        res += "\tSubstrate: %s\n" % self.substrate
-        res += "\tMod target: %s\n" % self.target
-        res += "\tValue: %s\n" % self.value
+        res += "\tEnzyme: {}\n".format(self.enzyme)
+        res += "\tSubstrate: {}\n".format(self.substrate)
+        res += "\tMod target: {}\n".format(self.target)
+        res += "\tValue: {}\n".format(self.value)
 
         if self.enzyme_bnd_region is not None:
             res += "\tEnzyme binding region: {}\n".format(
@@ -241,8 +270,10 @@ class TransModification(Modification):
         if self.substrate_bnd_site is not None:
             res += "\tSubstrate binding site: {}\n".format(
                 self.substrate_bnd_site)
-
-        res += "\tDirect? %s\n" % self.direct
+        if self.rate is not None:
+            res += "\tRate: {}\n".format(self.rate)
+        if self.annotation is not None:
+            res += "\tAnnotation: {}\n".format(self.annotation)
         return res
 
     def __repr__(self):
@@ -267,5 +298,9 @@ class TransModification(Modification):
         if self.substrate_bnd_site is not None:
             res += ", substrate_bnd_site={}".format(
                 self.substrate_bnd_site.__repr__())
+        if self.rate is not None:
+            res += ", rate={}".format(self.rate)
+        if self.annotation is not None:
+            res += ", annotation={}".format(str(self.annotation))
         res += ")"
         return res
