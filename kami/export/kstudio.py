47ae739d99b366be801e3eeff93926b75a123ace
Sebastien Legare <sebastien.legare.2@gmail.com>
2018-05-29 18:34:48 +0200

    Added node attributes

--- kami/export/kstudio.py
+++ kami/export/kstudio.py
@@ -25,12 +25,12 @@ def ag_to_edge_list(hierarchy, agent_ids="hgnc_symbol"):
     return edge_list
 
 
-def get_studio_v1(hierarchy,
+def to_kamistudio(hierarchy,
                   gene_label="hgnc_symbol", region_label="label"):
     """
     Convert a Kami hierarchy to a dictionary formatted for the old KamiStudio.
     To convert a Kami model into a KamiStudio readable file:
-    yourmodel = kami_hierarchy.get_studio_v1()
+    yourmodel = to_kamistudio(kami_hierarchy)
     json.dump(yourmodel, outfile, indent=4, sort_keys=False)
     """
 
@@ -40,14 +40,7 @@ def get_studio_v1(hierarchy,
         of labels chosen on get_studio_v1 call.
         """
 
-        label = node_typ  # For bnd, brk, mod, syn and deg.
-        if node_typ == "half-act":
-            label = "half"
-        if node_typ == "is_bnd":
-            label = "bound"
-        if node_typ == "is_free":
-            label = "free"
-        # Labeling of entities is more subtle.
+        label = node_typ
         if node_typ == "gene":
             try:
                 field = (hierarchy.graph[graph_level]
@@ -98,8 +91,6 @@ def get_studio_v1(hierarchy,
             else:
                 label = '%s%s' % (aa, loc)
         if node_typ == "state":
-            #underscore = node_id.rfind("_")
-            #state_name = node_id[underscore + 1:] 
             field = (hierarchy.graph[graph_level]
                      .node[node_id]["name"])
             state_name = list(field)[0]
@@ -133,27 +124,23 @@ def get_studio_v1(hierarchy,
     top_graph = {}
 
     nodes = []
-    for node_type in ["component", "action", "state", "test"]:
-        node = {"id": "", "type": "",
-                "attrs": {"val": {"numSet": {"neg_list": []},
-                                  "strSet": {"neg_list": []}}}}
+    for node_type in ["component", "action", "state"]:
+        node = {"id": "", "type": "", "attrs": {}}
         node["id"] = node_type
         nodes.append(node)
     top_graph["nodes"] = nodes
 
-    edges = [{"from": "component", "to": "component", "attrs": {}},
-             {"from": "component", "to": "action",    "attrs": {}},
-             {"from": "component", "to": "test", "attrs": {}},
-             {"from": "action",    "to": "component", "attrs": {}},
-             {"from": "action",    "to": "state", "attrs": {}},
-             {"from": "state",     "to": "component", "attrs": {}}]
+    edges = [{"from": "component", "to": "component", "attrs": {"pouf":"paf"}},
+             {"from": "component", "to": "action",    "attrs": {"pouf":"paf"}},
+             {"from": "action",    "to": "component", "attrs": {"pouf":"paf"}},
+             {"from": "action",    "to": "state", "attrs": {"pouf":"paf"}},
+             {"from": "state",     "to": "component", "attrs": {"pouf":"paf"}}]
     top_graph["edges"] = edges
 
     positions = {
-        "action":    {"x": 818.1, "y": 530.9},
-        "component": {"x": 621.4, "y": 435.1},
-        "state":     {"x": 801.8, "y": 298.7},
-        "test":      {"x": 585.1, "y": 517.5}
+        "action":    {"x": 820, "y": 530},
+        "component": {"x": 620, "y": 435},
+        "state":     {"x": 800, "y": 300}
     }
     attributes = {"name": "kami_base", "positions": positions}
     top_graph["attributes"] = attributes
@@ -170,20 +157,14 @@ def get_studio_v1(hierarchy,
     kami_meta_model["name"] = "kami"
     top_graph = {}
 
-    kami_typing = {"bnd": "action", "is_free": "test", "state": "state",
-                   "is_bnd": "test", "brk": "action", "mod": "action",
+    kami_typing = {"bnd": "action", "state": "state", "mod": "action",
                    "residue": "component", "deg": "action", "syn": "action",
-                   "half-act": "component", "gene": "component",
-                   "region": "component", "site": "component"}
+                   "gene": "component", "region": "component", "site": "component"}
 
     nodes = []
     for kami_node in hierarchy.graph['kami'].nodes():
-        node = {"id": "", "type": "",
-                "attrs": {"val": {"numSet": {"neg_list": []},
-                                  "strSet": {"neg_list": []}}}}
+        node = {"id": "", "type": "", "attrs": {}}
         node_id = kami_node
-        if kami_node == "locus":  # Rename locus to half-action.
-            node_id = "half-act"
         node["id"] = node_id
         node["type"] = kami_typing[node_id]
         nodes.append(node)
@@ -192,12 +173,8 @@ def get_studio_v1(hierarchy,
     edges = []
     for kami_edge in hierarchy.graph['kami'].edges():
         source = kami_edge[0]
-        if source == "locus":
-            source = "half-act"
         target = kami_edge[1]
-        if target == "locus":
-            target = "half-act"
-        edge = {"from": source, "to": target, "attrs": {}}
+        edge = {"from": source, "to": target, "attrs": {"pouf":"paf"}}
         edges.append(edge)
     top_graph["edges"] = edges
 
@@ -232,32 +209,30 @@ def get_studio_v1(hierarchy,
     nodes = []
     for ag_node in hierarchy.graph['action_graph'].nodes():
         node_type = action_graph_typing[ag_node]
-        if node_type == "locus":
-            node_type = "half-act"
         node_label, counters = find_studio_label(ag_node,
                                                  node_type,
                                                  counters,
                                                  "action_graph")
         label_tracker[ag_node] = node_label
+        # ----- Get all attributes of the action graph node (except rate) -----
         attrs = {}
-        vals = "empty"
-        if node_type == "mod":
-            vals = list(hierarchy.graph['action_graph']
-                        .node[ag_node]['value'])
-        elif node_type == "state":
-            vals = list(hierarchy.graph['action_graph']
-                        .node[ag_node].values())[0]
-        if vals != "empty":
-            value_list = []
-            for val in vals:
-                if val is True:
-                    val = "True"
-                if val is False:
-                    val = "False"
-                value_list.append(val)
-            attrs = {"val": {"numSet": {"pos_list": []},
-                             "strSet": {"pos_list": []}}}
-            attrs["val"]["strSet"]["pos_list"] = value_list
+        attributes = list(hierarchy.graph['action_graph']
+                        .node[ag_node].keys())
+        for attribute in attributes:
+            if attribute != "rate":
+                attrs[attribute] = {"numSet": {"pos_list": []},
+                                    "strSet": {"pos_list": []}}
+                vals = list(hierarchy.graph['action_graph']
+                            .node[ag_node][attribute])
+                value_list = []
+                for val in vals:
+                    if val is True:
+                        val = "True"
+                    if val is False:
+                        val = "False"
+                    value_list.append(val)
+                attrs[attribute]["strSet"]["pos_list"] = value_list
+        # ---------------------------------------------------------
         node = {"id": node_label, "type": node_type, "attrs": attrs}
         nodes.append(node)
     top_graph["nodes"] = nodes
@@ -266,6 +241,27 @@ def get_studio_v1(hierarchy,
     for ag_edge in hierarchy.graph['action_graph'].edges():
         source_label = label_tracker[ag_edge[0]]
         target_label = label_tracker[ag_edge[1]]
+        ## Uncomment when edge attributes will work in KAMIStudio
+        ## ----- Get all attributes of the action graph edge --------------------
+        #attrs = {}
+        #attributes = list(hierarchy.graph['action_graph']
+        #                  .edge[ag_edge[0]][ag_edge[1]].keys())
+        #for attribute in attributes:
+        #    if attribute != "rate":
+        #        attrs[attribute] = {"numSet": {"pos_list": []},
+        #                            "strSet": {"pos_list": []}}
+        #        vals = list(hierarchy.graph['action_graph']
+        #                    .edge[ag_edge[0]][ag_edge[1]][attribute])
+        #        value_list = []
+        #        for val in vals:
+        #            if val is True:
+        #                val = "True"
+        #            if val is False:
+        #                val = "False"
+        #            value_list.append(val)
+        #        attrs[attribute]["strSet"]["pos_list"] = value_list
+        ## ---------------------------------------------------------
+        #edge = {"from": source_label, "to": target_label, "attrs": attrs}
         edge = {"from": source_label, "to": target_label, "attrs": {}}
         edges.append(edge)
     top_graph["edges"] = edges
@@ -295,21 +291,23 @@ def get_studio_v1(hierarchy,
         for nugget_node in hierarchy.graph[nugget_id].nodes():
             node_type_ag = nugget_graph_typing[nugget_node]
             node_metatype = action_graph_typing[node_type_ag]
-            if node_metatype == "locus":
-                node_metatype = "half-act"
             node_label, ngt_counters = find_studio_label(nugget_node,
                                                          node_metatype,
                                                          ngt_counters,
                                                          nugget_id)
             node_type_studio = label_tracker[node_type_ag]
             nugget_label_tracker[nugget_node] = node_label
-            attrs = {}
 
-            vals, tests, bnd_types = "empty", "empty", "empty"
-            if node_metatype == "mod":
-                vals = list(hierarchy.graph[nugget_id]
-                            .node[nugget_node]['value'])
-                if vals != "empty":
+            # ----- Get all attributes of the nugget node (except rate) -----
+            attrs = {}
+            attributes = list(hierarchy.graph[nugget_id]
+                            .node[nugget_node].keys())
+            for attribute in attributes:
+                if attribute != "rate":
+                    attrs[attribute] = {"numSet": {"pos_list": []},
+                                        "strSet": {"pos_list": []}}
+                    vals = list(hierarchy.graph[nugget_id]
+                                .node[nugget_node][attribute])
                     value_list = []
                     for val in vals:
                         if val is True:
@@ -317,51 +315,8 @@ def get_studio_v1(hierarchy,
                         if val is False:
                             val = "False"
                         value_list.append(val)
-                    attrs = {"value": {"numSet": {"pos_list": []},
-                                       "strSet": {"pos_list": []}}}
-                    attrs["value"]["strSet"]["pos_list"] = value_list
-            elif node_metatype == "state":
-                tests = list(hierarchy.graph[nugget_id]
-                             .node[nugget_node]['test'])
-                if tests != "empty":
-                    test_list = []
-                    for test in tests:
-                        if test is True:
-                            test = "True"
-                        if test is False:
-                            test = "False"
-                        test_list.append(test)
-                    attrs = {"test": {"numSet": {"pos_list": []},
-                                      "strSet": {"pos_list": []}}}
-                    attrs["test"]["strSet"]["pos_list"] = test_list
-            elif node_metatype == "bnd":
-                attrs = {"test": {"numSet": {"pos_list": []},
-                                  "strSet": {"pos_list": []}},
-                         "type": {"numSet": {"pos_list": []},
-                                  "strSet": {"pos_list": []}}}
-                tests = list(hierarchy.graph[nugget_id]
-                            .node[nugget_node]['test'])
-                if tests != "empty":
-                    test_list = []
-                    for test in tests:
-                        if test is True:
-                            test = "True"
-                        if test is False:
-                            test = "False"
-                        test_list.append(test)
-                    attrs["test"]["strSet"]["pos_list"] = test_list
-                bnd_types = list(hierarchy.graph[nugget_id]
-                            .node[nugget_node]['type'])
-                if bnd_types != "empty":
-                   type_list = []
-                   for bnd_type in bnd_types:
-                       if bnd_type is True:
-                           bnd_type = "True"
-                       if bnd_type is False:
-                           bnd_type = "False"
-                       type_list.append(bnd_type)
-                   attrs["type"]["strSet"]["pos_list"] = type_list
-
+                    attrs[attribute]["strSet"]["pos_list"] = value_list
+            # ---------------------------------------------------------
             node = {"id": node_label, "type": node_type_studio,
                     "attrs": attrs}
             nodes.append(node)
@@ -385,6 +340,27 @@ def get_studio_v1(hierarchy,
         for nugget_edge in hierarchy.graph[nugget_id].edges():
             source_label = nugget_label_tracker[nugget_edge[0]]
             target_label = nugget_label_tracker[nugget_edge[1]]
+            ## Uncomment when edge attributes will work in KAMIStudio
+            ## ----- Get all attributes of the nugget edge --------------------
+            #attrs = {}
+            #attributes = list(hierarchy.graph[nugget_id]
+            #                  .edge[nugget_edge[0]][nugget_edge[1]].keys())
+            #for attribute in attributes:
+            #    if attribute != "rate":
+            #        attrs[attribute] = {"numSet": {"pos_list": []},
+            #                            "strSet": {"pos_list": []}}
+            #        vals = list(hierarchy.graph[nugget_id]
+            #                    .edge[nugget_edge[0]][nugget_edge[1]][attribute])
+            #        value_list = []
+            #        for val in vals:
+            #            if val is True:
+            #                val = "True"
+            #            if val is False:
+            #                val = "False"
+            #            value_list.append(val)
+            #        attrs[attribute]["strSet"]["pos_list"] = value_list
+            ## ---------------------------------------------------------
+            #edge = {"from": source_label, "to": target_label, "attrs": attrs}
             edge = {"from": source_label, "to": target_label, "attrs": {}}
             edges.append(edge)
         top_graph["edges"] = edges
