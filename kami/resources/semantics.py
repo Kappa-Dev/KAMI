68aa9b13f7e663b3a00c635eb852782a4200fd57
Eugenia Oshurko <yarutoua@gmail.com>
2018-11-16 09:52:58 +0100

    Bugfixes in semantic ag and nugget def

--- kami/resources/semantics.py
+++ kami/resources/semantics.py
@@ -5,22 +5,23 @@ import networkx as nx
 action_graph = {
     "nodes": [
         "protein_kinase",
-        ("activity", {"name": {"activity"}, "test": {True}}),
+        ("protein_kinase_activity", {"name": {"activity"}, "test": {True}}),
         ("phospho", {"value": {True}}),
         ("phospho_state", {"name": {"phosphorylation"}, "test": {True, False}}),
         ("phospho_target_residue", {"aa": {"S", "T", "Y"}, "test": {True}}),
         "phosphatase",
+        ("phosphatase_activity", {"name": {"activity"}, "test": {True}}),
         ("dephospho", {"value": {False}}),
         "pY_site",
         ("sh2_domain_pY_bnd", {"type": "do", "test": True}),
         "sh2_domain",
     ],
     "edges": [
-        ("activity", "protein_kinase"),
+        ("protein_kinase_activity", "protein_kinase"),
         ("protein_kinase", "phospho"),
         ("phospho", "phospho_state"),
         ("phospho_state", "phospho_target_residue"),
-        ("activity", "phosphatase"),
+        ("phosphatase_activity", "phosphatase"),
         ("phosphatase", "dephospho"),
         ("dephospho", "phospho_state"),
         ("phospho_target_residue", "pY_site"),
@@ -31,11 +32,13 @@ action_graph = {
 
 sag_meta_typing = {
     "protein_kinase": "region",
-    "activity": "state",
+    "protein_kinase_activity": "state",
     "phospho": "mod",
     "phospho_state": "state",
     "phospho_target_residue": "residue",
     "phosphatase": "region",
+    "phosphatase_activity": "state",
+    "protein_kinase_activity": "state",
     "dephospho": "mod",
     "pY_site": "site",
     "sh2_domain_pY_bnd": "bnd",
@@ -48,29 +51,29 @@ phosphorylation = {
         ["protein_kinase",
          ("protein_kinase_activity", {"name": {"activity"}, "test": {True}}),
          ("phospho", {"value": {True}}),
-         ("target_state", {"name": {"phosphorylation"}, "test": {False}}),
-         ("target_residue", {"aa": {"S", "T", "Y"}, "test": {True}})],
+         ("phospho_state", {"name": {"phosphorylation"}, "test": {False}}),
+         ("phospho_target_residue", {"aa": {"S", "T", "Y"}, "test": {True}})],
     "edges":
         [("protein_kinase_activity", "protein_kinase"),
          ("protein_kinase", "phospho"),
-         ("phospho", "target_state"),
-         ("target_state", "target_residue")]
+         ("phospho", "phospho_state"),
+         ("phospho_state", "phospho_target_residue")]
 }
 
 phosphorylation_meta_typing = {
     "protein_kinase": "region",
     "protein_kinase_activity": "state",
     "phospho": "mod",
-    "target_state": "state",
-    "target_residue": "residue"
+    "phospho_state": "state",
+    "phospho_target_residue": "residue"
 }
 
 phosphorylation_semantic_AG = {
     "protein_kinase": "protein_kinase",
-    "protein_kinase_activity": "activity",
+    "protein_kinase_activity": "protein_kinase_activity",
     "phospho": "phospho",
-    "target_state": "phospho_state",
-    "target_residue": "phospho_target_residue"
+    "phospho_state": "phospho_state",
+    "phospho_target_residue": "phospho_target_residue"
 }
 
 # Dephosphorylation semantic nugget
@@ -79,14 +82,14 @@ dephosphorylation = {
         "phosphatase",
         ("phosphatase_activity", {"name": {"activity"}, "test": {True}}),
         ("dephospho", {"value": False}),
-        ("target_state", {"name": {"phosphorylation"}, "test": {True}}),
-        ("target_residue", {"aa": {"S", "T", "Y"}, "test": {True}})
+        ("phospho_state", {"name": {"phosphorylation"}, "test": {True}}),
+        ("phospho_target_residue", {"aa": {"S", "T", "Y"}, "test": {True}})
     ],
     "edges": [
         ("phosphatase_activity", "phosphatase"),
         ("phosphatase", "dephospho"),
-        ("dephospho", "target_state"),
-        ("target_state", "target_residue")
+        ("dephospho", "phospho_state"),
+        ("phospho_state", "phospho_target_residue")
     ]
 }
 
@@ -94,16 +97,16 @@ dephosphorylation_meta_typing = {
     "phosphatase": "region",
     "phosphatase_activity": "state",
     "dephospho": "mod",
-    "target_state": "state",
-    "target_residue": "residue"
+    "phospho_state": "state",
+    "phospho_target_residue": "residue"
 }
 
 dephosphorylation_semantic_AG = {
     "phosphatase": "phosphatase",
-    "phosphatase_activity": "activity",
+    "phosphatase_activity": "phosphatase_activity",
     "dephospho": "dephospho",
-    "target_state": "phospho_state",
-    "target_residue": "phospho_target_residue"
+    "phospho_state": "phospho_state",
+    "phospho_target_residue": "phospho_target_residue"
 }
 
 # SH2 - pY binding semantic nugget
