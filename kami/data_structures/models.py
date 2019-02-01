d3cf2aebd6e862d8cabc2c542e0378b2482df5f2
Eugenia Oshurko <yarutoua@gmail.com>
2019-02-01 18:43:51 +0100

    Started a data structure for instantiated model

--- kami/data_structures/models.py
+++ kami/data_structures/models.py
@@ -1,2 +1,24 @@
+"""Collection of data structures for instantiated KAMI models."""
+
+
 class KamiModel(object):
-    pass
+    """Class for instantiated KAMI models.
+
+    Attributes
+    ----------
+    _backend : str, "networkx" or "neo4j"
+    _seed_genes :
+    _hierarchy :
+    creation_time :
+    annotation :
+    definitions :
+    action_graph :
+    nugget :
+    mod_template :
+    bnd_templae :
+    """
+
+    def __init__(self, corpora, definitions,
+                 seed_genes=None, annotation=None):
+        """Instantiate a model from the input corpus and protein defs."""
+        # Copy the hierarchy
