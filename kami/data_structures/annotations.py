465aa59af8c18958284e51ec4a246eca2db3165d
Eugenia Oshurko <yarutoua@gmail.com>
2019-03-20 15:44:23 +0100

    Added annotation from json

--- kami/data_structures/annotations.py
+++ kami/data_structures/annotations.py
@@ -2,7 +2,7 @@
 
 
 class CorpusAnnotation:
-    """."""
+    """Data structure for annotation."""
 
     def __init__(self, name=None, desc=None, organism=None, text=None):
         """."""
@@ -18,3 +18,19 @@ class CorpusAnnotation:
             "organism": self.organism,
             "annotation": self.text
         }
+
+    @classmethod
+    def from_json(cls, json_data):
+        name = None
+        desc = None
+        organism = None
+        text = None
+        if "name" in json_data.keys():
+            name = json_data["name"]
+        if "desc" in json_data.keys():
+            desc = json_data["desc"]
+        if "organism" in json_data.keys():
+            organism = json_data["organism"]
+        if "text" in json_data.keys():
+            text = json_data["text"]
+        return cls(name, desc, organism, text)
